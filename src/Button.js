306fc751a59750859446e4857f50021b3e733760
Müller Lisa <mli@TIM-M093.local>
2019-01-04 17:35:50 +0100

    rm comments & console.logs

--- src/Button.js
+++ src/Button.js
@@ -1,14 +1,8 @@
 import React, {Component} from 'react';
 import './Button.css';
 
-// const Button = ({direction}) => {
-//   return <div className={`button-${direction} button`}>button</div>
-// }
-
 class Button extends Component {
   handleClick = (e) => {
-    //console.log(this.direction);
-    //console.log(this.props.direction);
     this.props.handleClick(this.props.direction);
   }
 
