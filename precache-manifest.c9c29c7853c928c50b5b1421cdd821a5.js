ee42c68409efeedc37003229eebc4bf2400c9888
Müller Lisa <mli@TIM-M093.local>
2019-01-04 16:47:23 +0100

    Updates

---
+++ precache-manifest.c9c29c7853c928c50b5b1421cdd821a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5fa56b8d3529967095c",
+    "url": "/react-snake/static/css/main.54d804d5.chunk.css"
+  },
+  {
+    "revision": "f5fa56b8d3529967095c",
+    "url": "/react-snake/static/js/main.f5fa56b8.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/react-snake/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "d1a1eebc9b5c85bbcb8a",
+    "url": "/react-snake/static/js/runtime~main.d1a1eebc.js"
+  },
+  {
+    "revision": "4d11cf7cb87604ceb0a7b5e4c014f4a4",
+    "url": "/react-snake/index.html"
+  }
+];
